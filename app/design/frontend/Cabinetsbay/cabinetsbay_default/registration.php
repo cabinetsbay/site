cb1bf8d4b3fc376c8e9f41aff5ca1d22a21f603a
Dmitrii Fediuk <dfediuk@gmail.com>
2023-11-25 01:06:46 +0300

    no message

---
+++ app/design/frontend/Cabinetsbay/cabinetsbay_default/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Sharapov A. <alexander@sharapov.biz>
+ * @link      http://www.sharapov.biz/
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
+ * Date: 30/09/2019
+ * Time: 14:55
+ */
+
+Magento\Framework\Component\ComponentRegistrar::register(
+  \Magento\Framework\Component\ComponentRegistrar::THEME,
+  'frontend/Cabinetsbay/cabinetsbay_default',
+    __DIR__
+);
