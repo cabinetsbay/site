8a3cbd2c815f8c4a0fa3e0c6a4e40047dd63ff0c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-24 23:46:32 +0100

    https://github.com/cabinetsbay/site/issues/121

--- app/design/frontend/Cabinetsbay/cabinetsbay_default/web/js/cabinetsbay.js
+++ app/design/frontend/Cabinetsbay/cabinetsbay_default/web/js/cabinetsbay.js
@@ -71,70 +71,72 @@ require([
 			}
 		});
 	})();
-	let $sortWrapper = $('#products_list');
-	let $sortPriceToggler = $('#dropdown-price');
-	$sortPriceToggler.find('.dropdown li').on('click', function (el) {
-		$sortPriceToggler.find('span.toggle > span').text('Price (' + el.target.innerHTML + ')');
-		setTimeout(function () {
-			$('.category-filter-dropdown').removeClass('active');
-		}, 500);
-		if(el.target.dataset.sortby == 'asc') {
-			$sortWrapper.find('li').sort(function(a, b) {
-				return +a.dataset.sorterprice - +b.dataset.sorterprice;
-			}).appendTo($sortWrapper);
-		}
-		else {
-			$sortWrapper.find('li').sort(function(a, b) {
-				return +b.dataset.sorterprice - +a.dataset.sorterprice;
-			}).appendTo($sortWrapper);
-		}
-	});
-	let $sortColorToggler = $('#dropdown-color');
-	$sortColorToggler.find('.dropdown li').on('click', function (el) {
-		$sortColorToggler.find('span.toggle > span').text('Color (' + el.target.innerHTML + ')');
-		setTimeout(function () {
-			$('.category-filter-dropdown').removeClass('active');
-		}, 500);
-		if (el.target.dataset.sortby == 'darker') {
-			$sortWrapper.find('li').sort(function(a, b) {
-				return +b.dataset.sortercolor - +a.dataset.sortercolor;
-			}).appendTo($sortWrapper);
-		}
-		else {
-			$sortWrapper.find('li').sort(function(a, b) {
-				return +a.dataset.sortercolor - +b.dataset.sortercolor;
-			}).appendTo($sortWrapper);
-		}
-	});
-	$sortColorToggler.find('ul > li:first').click();
-	let $sortStyleToggler = $('#dropdown-style');
-	$sortStyleToggler.find('.dropdown li').on('click', function (el) {
-		$sortStyleToggler.find('span.toggle > span').text('Style (' + el.target.innerHTML + ')');
-		setTimeout(function () {
-			$('.category-filter-dropdown').removeClass('active');
-		}, 500);
-		if(el.target.innerHTML == 'All') {
-			$sortWrapper.find('li').show();
-		}
-		else {
-			$sortWrapper.find('li').hide();
-			$sortWrapper.find('li[data-sorterstyle="'+el.target.innerHTML+'"]').show();
-		}
-	});
-	let $sortConstructionToggler = $('#dropdown-construction');
-	$sortConstructionToggler.find('.dropdown li').on('click', function (el) {
-		$sortConstructionToggler.find('span.toggle > span').text('Cabinet Construction (' + el.target.innerHTML + ')');
-		setTimeout(function () {
-			$('.category-filter-dropdown').removeClass('active');
-		}, 500);
-		if(el.target.innerHTML == 'All') {
-			$sortWrapper.find('li').show();
-		}
-		else {
-			$sortWrapper.find('li').hide();
-			$sortWrapper.find('li[data-sorterconstruction="'+el.target.innerHTML+'"]').show();
-		}
-	});
+	(function() {
+		let $sortWrapper = $('#products_list');
+		let $sortPriceToggler = $('#dropdown-price');
+		$sortPriceToggler.find('.dropdown li').on('click', function (el) {
+			$sortPriceToggler.find('span.toggle > span').text('Price (' + el.target.innerHTML + ')');
+			setTimeout(function () {
+				$('.category-filter-dropdown').removeClass('active');
+			}, 500);
+			if(el.target.dataset.sortby == 'asc') {
+				$sortWrapper.find('li').sort(function(a, b) {
+					return +a.dataset.sorterprice - +b.dataset.sorterprice;
+				}).appendTo($sortWrapper);
+			}
+			else {
+				$sortWrapper.find('li').sort(function(a, b) {
+					return +b.dataset.sorterprice - +a.dataset.sorterprice;
+				}).appendTo($sortWrapper);
+			}
+		});
+		let $sortColorToggler = $('#dropdown-color');
+		$sortColorToggler.find('.dropdown li').on('click', function (el) {
+			$sortColorToggler.find('span.toggle > span').text('Color (' + el.target.innerHTML + ')');
+			setTimeout(function () {
+				$('.category-filter-dropdown').removeClass('active');
+			}, 500);
+			if (el.target.dataset.sortby == 'darker') {
+				$sortWrapper.find('li').sort(function(a, b) {
+					return +b.dataset.sortercolor - +a.dataset.sortercolor;
+				}).appendTo($sortWrapper);
+			}
+			else {
+				$sortWrapper.find('li').sort(function(a, b) {
+					return +a.dataset.sortercolor - +b.dataset.sortercolor;
+				}).appendTo($sortWrapper);
+			}
+		});
+		$sortColorToggler.find('ul > li:first').click();
+		let $sortStyleToggler = $('#dropdown-style');
+		$sortStyleToggler.find('.dropdown li').on('click', function (el) {
+			$sortStyleToggler.find('span.toggle > span').text('Style (' + el.target.innerHTML + ')');
+			setTimeout(function () {
+				$('.category-filter-dropdown').removeClass('active');
+			}, 500);
+			if(el.target.innerHTML == 'All') {
+				$sortWrapper.find('li').show();
+			}
+			else {
+				$sortWrapper.find('li').hide();
+				$sortWrapper.find('li[data-sorterstyle="'+el.target.innerHTML+'"]').show();
+			}
+		});
+		let $sortConstructionToggler = $('#dropdown-construction');
+		$sortConstructionToggler.find('.dropdown li').on('click', function (el) {
+			$sortConstructionToggler.find('span.toggle > span').text('Cabinet Construction (' + el.target.innerHTML + ')');
+			setTimeout(function () {
+				$('.category-filter-dropdown').removeClass('active');
+			}, 500);
+			if(el.target.innerHTML == 'All') {
+				$sortWrapper.find('li').show();
+			}
+			else {
+				$sortWrapper.find('li').hide();
+				$sortWrapper.find('li[data-sorterconstruction="'+el.target.innerHTML+'"]').show();
+			}
+		});
+	})();
 	if(screen.width < 680 && $('.products.wrapper.list').hasClass('products-list')) {
 		$('.products.wrapper.list').removeClass('products-list').addClass('products-grid');
 	}
