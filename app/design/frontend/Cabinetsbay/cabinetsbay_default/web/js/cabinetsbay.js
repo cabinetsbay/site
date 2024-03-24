86b23fe5b562ba11ec65ca737d9ea90c5fb0bdf8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-24 23:43:14 +0100

    https://github.com/cabinetsbay/site/issues/121

--- app/design/frontend/Cabinetsbay/cabinetsbay_default/web/js/cabinetsbay.js
+++ app/design/frontend/Cabinetsbay/cabinetsbay_default/web/js/cabinetsbay.js
@@ -51,25 +51,28 @@ require([
 			$('#overview.category-tab').addClass('active');
 		}
 	}
-	if ($('#category-gallery').length > 0) {
-		$('#category-gallery').lightSlider({
-			adaptiveHeight: true,
-			auto: false,
-			autoWidth: false,
-			enableDrag: false,
-			gallery: true,
-			galleryMargin: 0,
-			item: 1,
-			loop: true,
-			slideMargin: 0,
-			speed: 700,
-			thumbItem: 3,
-			onSliderLoad: function (el) {
-				$('#category-gallery').removeClass('cS-hidden');
-				el.lightGallery({download: false, selector: '#category-gallery .lslide'});
-			}
-		});
-	}
+	(function() {
+		const $g = $('#category-gallery');
+		if ($g.length) {
+			$g.lightSlider({
+				adaptiveHeight: true,
+				auto: false,
+				autoWidth: false,
+				enableDrag: false,
+				gallery: true,
+				galleryMargin: 0,
+				item: 1,
+				loop: true,
+				slideMargin: 0,
+				speed: 700,
+				thumbItem: 3,
+				onSliderLoad: function (el) {
+					$('#category-gallery').removeClass('cS-hidden');
+					el.lightGallery({download: false, selector: '#category-gallery .lslide'});
+				}
+			});
+		}
+	})();
 	let $sortWrapper = $('#products_list');
 	let $sortPriceToggler = $('#dropdown-price');
 	$sortPriceToggler.find('.dropdown li').on('click', function (el) {
