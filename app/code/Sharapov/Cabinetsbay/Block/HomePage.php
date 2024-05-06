6c2c55bf466940b47d5dd01c4e4b36d0b3833803
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-06 17:59:30 +0100

    https://github.com/cabinetsbay/site/issues/146

--- app/code/Sharapov/Cabinetsbay/Block/HomePage.php
+++ app/code/Sharapov/Cabinetsbay/Block/HomePage.php
@@ -1,21 +1,11 @@
 <?php
-/**
- * @copyright Sharapov A. <alexander@sharapov.biz>
- * @link      http://www.sharapov.biz/
- * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
- * Date: 02/10/2019
- * Time: 20:40
- */
-
 namespace Sharapov\Cabinetsbay\Block;
-
-class HomePage extends \Magento\Framework\View\Element\Template
-{
-
+# 2024-05-06 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Refactor `app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Theme/templates/homepage.phtml`":
+# https://github.com/cabinetsbay/site/issues/146
+class HomePage extends \Magento\Framework\View\Element\Template {
 	const XML_CONFIG_POPULAR_PRODUCTS = 'cabinetsbay_settings/general/popular_products';
-
 	private $scopeConfig;
-
 	/**
 	 * Notification constructor.
 	 *
@@ -35,27 +25,19 @@ class HomePage extends \Magento\Framework\View\Element\Template
 	/**
 	 * @return array
 	 */
-	function getPopularProducts(): array
-	{
+	function getPopularProducts():array {
 		$ids = $this->scopeConfig->getValue(self::XML_CONFIG_POPULAR_PRODUCTS,
 			\Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-
 		$ids = ($ids) ? explode(",", $ids) : [];
 		$ids = array_slice($ids, 0, 3);
 		$popularCategories = [];
-
-		$objectManager
-			= \Magento\Framework\App\ObjectManager::getInstance(); // Instance of Object Manager
-		$categoryFactory
-			= $objectManager->get('\Magento\Catalog\Model\CategoryFactory');// Instance of Category Model
-
+		$objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // Instance of Object Manager
+		$categoryFactory = $objectManager->get('\Magento\Catalog\Model\CategoryFactory');// Instance of Category Model
 		foreach ($ids as $id) {
 			array_push($popularCategories,
 				$categoryFactory->create()->load($id));
 		}
-
 		unset($categoryFactory, $objectManager, $ids);
-
 		return $popularCategories;
 	}
 }
