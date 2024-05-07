f9d5e044551a4d26a17b2fbfe8b4d5569618e4e1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-07 12:16:27 +0100

    https://github.com/cabinetsbay/site/issues/146

--- app/code/Sharapov/Cabinetsbay/Block/HomePage.php
+++ app/code/Sharapov/Cabinetsbay/Block/HomePage.php
@@ -9,14 +9,7 @@ class HomePage extends \Magento\Framework\View\Element\Template {
 	 * Refactor `app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Theme/templates/homepage.phtml`":
 	 * https://github.com/cabinetsbay/site/issues/146
 	 */
-	function popular():array {
-		$ids = array_slice(df_csv_parse_int(df_cfg('cabinetsbay_settings/general/popular_products')), 0, 3);
-		$r = [];
-		$objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // Instance of Object Manager
-		$categoryFactory = $objectManager->get('\Magento\Catalog\Model\CategoryFactory');// Instance of Category Model
-		foreach ($ids as $id) {
-			array_push($r, $categoryFactory->create()->load($id));
-		}
-		return $r;
-	}
+	function popular():array {return df_category_c(array_slice(df_csv_parse_int(df_cfg(
+		'cabinetsbay_settings/general/popular_products'
+	)), 0, 3));}
 }
