c3b8f7815a52ccf4bbdacef0e9fda64e389223da
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-07 11:47:16 +0100

    https://github.com/cabinetsbay/site/issues/146

--- app/code/Sharapov/Cabinetsbay/Block/HomePage.php
+++ app/code/Sharapov/Cabinetsbay/Block/HomePage.php
@@ -4,25 +4,10 @@ namespace Sharapov\Cabinetsbay\Block;
 # "Refactor `app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Theme/templates/homepage.phtml`":
 # https://github.com/cabinetsbay/site/issues/146
 class HomePage extends \Magento\Framework\View\Element\Template {
-	private $scopeConfig;
 	/**
-	 * Notification constructor.
-	 *
-	 * @param \Magento\Framework\View\Element\Template\Context $context
-	 * @param \Magento\Checkout\Model\Session                  $checkoutSession
-	 * @param array                                            $data
-	 */
-	function __construct(
-		\Magento\Framework\View\Element\Template\Context $context,
-		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-		array $data = []
-	) {
-		parent::__construct($context, $data);
-		$this->scopeConfig = $scopeConfig;
-	}
-
-	/**
-	 * @return array
+	 * 2024-05-07 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * Refactor `app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Theme/templates/homepage.phtml`":
+	 * https://github.com/cabinetsbay/site/issues/146
 	 */
 	function getPopularProducts():array {
 		$ids = df_cfg('cabinetsbay_settings/general/popular_products');
