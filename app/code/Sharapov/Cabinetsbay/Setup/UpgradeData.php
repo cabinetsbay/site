6e192e20a9ab50986a1c4a5f7a22e17278b8b9c0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-19 05:33:41 +0100

    https://github.com/cabinetsbay/site/issues/98

--- app/code/Sharapov/Cabinetsbay/Setup/UpgradeData.php
+++ app/code/Sharapov/Cabinetsbay/Setup/UpgradeData.php
@@ -15,7 +15,7 @@ class UpgradeData implements UpgradeDataInterface {
 	function upgrade(ISetup $setup, IContext $context) {
 		$setup->startSetup();
 		if(version_compare($context->getVersion(), '1.0.1') < 0) {
-			self::a('cb_kitchen_type', ['input' => 'text', 'type' => 'varchar']);
+			self::a(A::KITCHEN_TYPE, ['input' => 'text', 'type' => 'varchar']);
 			df_map(
 				[__CLASS__, 'a']
 				,['cb_assembly', 'cb_specs', 'cb_styles']
