67841d3be5c7666092460a2a3e07a70e4c75ec0e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-19 05:14:23 +0100

    https://github.com/cabinetsbay/site/issues/98

--- app/code/Sharapov/Cabinetsbay/Setup/UpgradeData.php
+++ app/code/Sharapov/Cabinetsbay/Setup/UpgradeData.php
@@ -11,81 +11,80 @@ class UpgradeData implements UpgradeDataInterface {
 	 * 2024-05-19 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "Refactor the `Sharapov_Cabinetsbay` module": https://github.com/cabinetsbay/site/issues/98
 	 */
-  function upgrade(ISetup $setup, IContext $context) {
-	$setup->startSetup();
-	$eavSetup = df_eav_setup(); /** @var EavSetup $eavSetup */
-	if(version_compare($context->getVersion(), '1.0.1') < 0) {
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_specs',
-		[
-		  'is_visible_on_front' => true,
-		  'wysiwyg_enabled'     => true
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_assembly',
-		[
-		  'is_visible_on_front' => true,
-		  'wysiwyg_enabled'     => true
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_styles',
-		[
-		  'is_visible_on_front' => true,
-		  'wysiwyg_enabled'     => true
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_kitchen_set',
-		[
-		  'is_visible_on_front' => true
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_kitchen_style',
-		[
-		  'is_visible_on_front' => true
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_kitchen_type',
-		[
-		  'type'                  => 'varchar',
-		  'label'                 => 'Construction Type',
-		  'input'                 => 'text',
-		  'required'              => false,
-		  'sort_order'            => 225,
-		  'global'                => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
-		  'group'                 => 'General Information',
-		  'wysiwyg_enabled'       => true,
-		  'is_used_in_grid'       => true,
-		  'is_visible_in_grid'    => true,
-		  'is_filterable_in_grid' => false,
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_kitchen_price',
-		[
-		  'is_visible_on_front' => true
-		]
-	  );
-	  $eavSetup->updateAttribute(
-		\Magento\Catalog\Model\Category::ENTITY,
-		'cb_door_sample_link',
-		[
-		  'is_visible_on_front' => true
-		]
-	  );
+	function upgrade(ISetup $setup, IContext $context) {
+		$setup->startSetup();
+		$eavSetup = df_eav_setup(); /** @var EavSetup $eavSetup */
+		if(version_compare($context->getVersion(), '1.0.1') < 0) {
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_specs',
+			[
+			'is_visible_on_front' => true,
+			'wysiwyg_enabled'     => true
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_assembly',
+			[
+			'is_visible_on_front' => true,
+			'wysiwyg_enabled'     => true
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_styles',
+			[
+			'is_visible_on_front' => true,
+			'wysiwyg_enabled'     => true
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_kitchen_set',
+			[
+			'is_visible_on_front' => true
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_kitchen_style',
+			[
+			'is_visible_on_front' => true
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_kitchen_type',
+			[
+			'type'                  => 'varchar',
+			'label'                 => 'Construction Type',
+			'input'                 => 'text',
+			'required'              => false,
+			'sort_order'            => 225,
+			'global'                => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+			'group'                 => 'General Information',
+			'wysiwyg_enabled'       => true,
+			'is_used_in_grid'       => true,
+			'is_visible_in_grid'    => true,
+			'is_filterable_in_grid' => false,
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_kitchen_price',
+			[
+			'is_visible_on_front' => true
+			]
+			);
+			$eavSetup->updateAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'cb_door_sample_link',
+			[
+			'is_visible_on_front' => true
+			]
+			);
+		}
+		$setup->endSetup();
 	}
-
-	$setup->endSetup();
-  }
 }
