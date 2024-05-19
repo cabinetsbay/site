4df78b1a23de8515e103bbda73846b668571c09e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-19 05:09:46 +0100

    https://github.com/cabinetsbay/site/issues/98

--- app/code/Sharapov/Cabinetsbay/Setup/UpgradeData.php
+++ app/code/Sharapov/Cabinetsbay/Setup/UpgradeData.php
@@ -25,7 +25,7 @@ class UpgradeData implements UpgradeDataInterface {
    * @param EavSetupFactory $eavSetupFactory
    */
   function __construct(EavSetupFactory $eavSetupFactory) {
-    $this->eavSetupFactory = $eavSetupFactory;
+	$this->eavSetupFactory = $eavSetupFactory;
   }
 
   /**
@@ -33,83 +33,83 @@ class UpgradeData implements UpgradeDataInterface {
    * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
    */
   function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
-    $setup->startSetup();
+	$setup->startSetup();
 
-    /** @var EavSetup $eavSetup */
-    $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+	/** @var EavSetup $eavSetup */
+	$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
 
-    if(version_compare($context->getVersion(), '1.0.1') < 0) {
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_specs',
-        [
-          'is_visible_on_front' => true,
-          'wysiwyg_enabled'     => true
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_assembly',
-        [
-          'is_visible_on_front' => true,
-          'wysiwyg_enabled'     => true
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_styles',
-        [
-          'is_visible_on_front' => true,
-          'wysiwyg_enabled'     => true
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_kitchen_set',
-        [
-          'is_visible_on_front' => true
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_kitchen_style',
-        [
-          'is_visible_on_front' => true
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_kitchen_type',
-        [
-          'type'                  => 'varchar',
-          'label'                 => 'Construction Type',
-          'input'                 => 'text',
-          'required'              => false,
-          'sort_order'            => 225,
-          'global'                => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
-          'group'                 => 'General Information',
-          'wysiwyg_enabled'       => true,
-          'is_used_in_grid'       => true,
-          'is_visible_in_grid'    => true,
-          'is_filterable_in_grid' => false,
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_kitchen_price',
-        [
-          'is_visible_on_front' => true
-        ]
-      );
-      $eavSetup->updateAttribute(
-        \Magento\Catalog\Model\Category::ENTITY,
-        'cb_door_sample_link',
-        [
-          'is_visible_on_front' => true
-        ]
-      );
-    }
+	if(version_compare($context->getVersion(), '1.0.1') < 0) {
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_specs',
+		[
+		  'is_visible_on_front' => true,
+		  'wysiwyg_enabled'     => true
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_assembly',
+		[
+		  'is_visible_on_front' => true,
+		  'wysiwyg_enabled'     => true
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_styles',
+		[
+		  'is_visible_on_front' => true,
+		  'wysiwyg_enabled'     => true
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_kitchen_set',
+		[
+		  'is_visible_on_front' => true
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_kitchen_style',
+		[
+		  'is_visible_on_front' => true
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_kitchen_type',
+		[
+		  'type'                  => 'varchar',
+		  'label'                 => 'Construction Type',
+		  'input'                 => 'text',
+		  'required'              => false,
+		  'sort_order'            => 225,
+		  'global'                => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+		  'group'                 => 'General Information',
+		  'wysiwyg_enabled'       => true,
+		  'is_used_in_grid'       => true,
+		  'is_visible_in_grid'    => true,
+		  'is_filterable_in_grid' => false,
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_kitchen_price',
+		[
+		  'is_visible_on_front' => true
+		]
+	  );
+	  $eavSetup->updateAttribute(
+		\Magento\Catalog\Model\Category::ENTITY,
+		'cb_door_sample_link',
+		[
+		  'is_visible_on_front' => true
+		]
+	  );
+	}
 
-    $setup->endSetup();
+	$setup->endSetup();
   }
 }
