f9a86e38b26084a6f1841a9b45030596bf879663
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 18:57:02 +0100

    https://github.com/cabinetsbay/site/issues/98

--- app/code/Sharapov/Cabinetsbay/Plugin/Category/DataProvider.php
+++ app/code/Sharapov/Cabinetsbay/Plugin/Category/DataProvider.php
@@ -11,12 +11,10 @@ final class DataProvider {
 	 * @used-by self::STUB()
 	 */
 	function afterPrepareMeta(Sb $sb, $result) {return array_merge_recursive($result, $this->_prepareFieldsMeta(
-		[
-			'content' => [
-				A::ASSEMBLY, A::KITCHEN_COLOR, A::KITCHEN_PRICE, A::KITCHEN_SET, A::KITCHEN_STYLE, A::KITCHEN_TYPE,
-				A::MATCHING_PRODUCTS, A::SPECS, A::STYLES
-			]
-		]
+		['content' => [
+			A::ASSEMBLY, A::KITCHEN_COLOR, A::KITCHEN_PRICE, A::KITCHEN_SET, A::KITCHEN_STYLE, A::KITCHEN_TYPE,
+			A::MATCHING_PRODUCTS, A::SPECS, A::STYLES
+		]]
 		,$sb->getAttributesMeta(df_eav_category())
 	));}
 
