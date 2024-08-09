d737fe223146d290aa33dc6f2f72ba754204f181
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-09 08:13:55 +0100

    https://github.com/cabinetsbay/site/issues/154

--- vendor/magento/framework/Filter/Template.php
+++ vendor/magento/framework/Filter/Template.php
@@ -106,6 +106,22 @@ class Template implements \Zend_Filter_Interface
     private $variableResolver;
 
     /**
+	 * 2024-08-09 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * 1) "Install the «ACSD-47578» security patch" https://github.com/cabinetsbay/site/issues/154
+	 * 2) https://github.com/magento/magento2/blob/2.4.7-p1/lib/internal/Magento/Framework/Filter/Template.php#L103-L106
+     * @var SignatureProvider|null
+     */
+    private $signatureProvider;
+
+    /**
+	 * 2024-08-09 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * 1) "Install the «ACSD-47578» security patch" https://github.com/cabinetsbay/site/issues/154
+	 * 2) https://github.com/magento/magento2/blob/2.4.7-p1/lib/internal/Magento/Framework/Filter/Template.php#L108-L111
+     * @var FilteringDepthMeter|null
+     */
+    private $filteringDepthMeter;
+
+    /**
      * @param StringUtils $string
      * @param array $variables
      * @param DirectiveProcessorInterface[] $directiveProcessors
